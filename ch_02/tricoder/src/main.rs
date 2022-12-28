c8a84a5f4a5bdcf16be10efd4dc0f013deedc832
ben lim <lim6112j@gmail.com>
2022-12-28 16:25:51 +0900

    working now

--- ch_02/tricoder/src/main.rs
+++ ch_02/tricoder/src/main.rs
@@ -1,4 +1,7 @@
 use std::{env, time::Duration};
+use model::Subdomain;
+use rayon::prelude::*;
+use reqwest::{blocking::Client, redirect};
 mod error;
 pub use error::Error;
 mod subdomains;
@@ -10,5 +13,30 @@ fn main() -> Result<(), anyhow::Error> {
     if args.len() != 2{
         return Err(Error::CliUsage.into());
     }
+    let target = args[1].as_str();
+    let http_timeout = Duration::from_secs(5);
+    let http_client = Client::builder()
+        .redirect(redirect::Policy::limited(4))
+        .timeout(http_timeout)
+        .build()?;
+
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(256)
+        .build()
+        .unwrap();
+    pool.install(|| {
+        let scan_result: Vec<Subdomain> = subdomains::enumerate(&http_client, target)
+            .unwrap()
+            .into_par_iter()
+            .map(ports::scan_ports)
+            .collect();
+        for subdomain in scan_result {
+            println!("{}:", &subdomain.domain);
+            for port in &subdomain.open_ports {
+                println!("     {}", port.port);
+            }
+            println!();
+        }
+    });
     Ok(())
 }
